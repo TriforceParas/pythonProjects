e34bbd70b77a39b36d1eb4b1cb60076744dcead8
TriforceParas <paras.tloz@gmail.com>
2024-01-06 20:56:47 +0530

    Updated Linked List

--- pythonDSA/LinkedList.py
+++ pythonDSA/LinkedList.py
@@ -9,6 +9,13 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
+    # CHeck if the Link List is ep
+    def isListEmpty(self):
+        if self.head is None:
+            return True
+        else:
+            return False
+    
     # Check the Size of the Linked List    
     def listLen(self):
         node = self.head
@@ -27,7 +34,7 @@ class LinkedList:
     # Insert Node a position
     def insertAt(self, newNode, position):
         # Checks if the postion is valid if not then returns an error message
-        if position < 0 or position > self.listLen:
+        if position < 0 or position > self.listLen():
             print("Invalid Position")
             return
         
@@ -41,7 +48,7 @@ class LinkedList:
         while True:
             if currentPostition == position: 
                 previousNode.next = newNode # Links before Current Node chain to new Node
-                newNode.new = currentNode # Links New Node to after Current Node Chain 
+                newNode.next = currentNode # Links New Node to after Current Node Chain 
                 break
             previousNode = currentNode # Stores before Current Node chain
             currentNode = currentNode.next # Stores after Current Node chain
@@ -60,6 +67,43 @@ class LinkedList:
                 lastNode = lastNode.next
             lastNode.next = newNode 
     
+    # Delete Head of the Linked List
+    def deleteHead(self):
+        if self.isListEmpty() is False:
+            previoushead = self.head
+            self.head = self.head.next
+            previoushead.next = None
+        else:
+            print('Linked List is empty')
+    
+    # Delete Node from End of Linked List
+    def deleteEnd(self):
+        lastNode = self.head
+        while lastNode.next is not None:
+            previousNode = lastNode
+            lastNode = lastNode.next
+        previousNode.next = None
+    
+    # Delete Node at a position on Linked List
+    def deleteAt(self, position):
+        if position < 0 or position > self.listLen():
+            print('Invalid Position')
+            return
+        if self.isListEmpty is False:
+            if position == 0:
+                self.deleteHead()
+                return
+            currentNode = self.head
+            currentPostition = 0
+            while True:
+                if currentPostition == position:
+                    previousNode.next = currentNode.next
+                    currentNode.next = None
+                    break
+                previousNode = currentNode
+                currentNode = currentNode.next
+                currentPostition += 1
+                    
     # To Print linked List
     def printList(self): 
         if self.head is None: 
@@ -90,6 +134,16 @@ linkedList.insertEnd(n5)
 # Assigns a new Head Node
 h1 = Node('Mumbai')
 linkedList.insertHead(h1)
+
+# Asssigns a new Node in between
+b1 = Node('Chennai')
+linkedList.insertAt(b1, 3)
+
+# Deleting Node from the end of the Linked List
+linkedList.deleteEnd()
+
+# Deleting Node from the Middle of the Linked List
+linkedList.deleteAt(2)
 
 # Printing Linked List
 linkedList.printList()
